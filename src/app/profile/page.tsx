53182fa9dcbf4514e69b247ffb84a41d600eabbf
snhrayk <ayaka21sunahara@gmail.com>
2024-12-30 23:30:54 +0900

    profileの上部分だけ完成。スクロールできるのかわかりにくいから、スクロールを促すものを追加したい

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,112 +1,119 @@
+import Image from "next/image";
 import Close from "../components/Close";
 import styles from "./Profile.module.scss";
+import figma from "../../../public/img/figma_profile_skill.png";
+// import ramen from "../../../public/img/ramen_profile_fav.png";
+// import music from "../../../public/img/music_profile_fav.png";
+// import paint from "../../../public/img/paint_profile_fav.png";
+// import movie from "../../../public/img/movies_profile_fav.png";
 
 export default function Profile() {
   return (
     <>
       <div className={styles.windowWrap}>
         <div className={styles.bar}>
-          <h1>Profile</h1>
+          <h1>
+            Profile (<span>私について</span>)
+          </h1>
           <Close />
         </div>
         <div className={styles.pageArea}>
-          {/* 私の情報 */}
-          <div className={styles.profileTop}>
-            <div className={styles.imgWrap}></div>
-            <div className={styles.textTop}>
-              <h2>profile card</h2>
-              <p className={styles.name}>
-                name<span>砂原彩花</span>
-              </p>
-              <div className={styles.schoolWrap}>
-                <p>education</p>
-                <p className={styles.school}>
-                  ECCコンピュータ専門学校 <br />
-                  <span>マルチメディア研究学科 / Webデザインコース</span>
+          {/* ここからページごとに違う */}
+          <div className={styles.profileTopWrap}>
+            {/* 私の個人情報 */}
+            <section className={styles.profileLeft}>
+              <div className={styles.imgWrap}></div>
+              <div className={styles.textTop}>
+                <h2>profile card</h2>
+                <p className={styles.name}>
+                  name<span>砂原彩花</span>
                 </p>
+                <div className={styles.schoolWrap}>
+                  <p>education</p>
+                  <p className={styles.school}>
+                    ECCコンピュータ専門学校 <br />
+                    <span>マルチメディア研究学科 / Webデザインコース</span>
+                  </p>
+                </div>
+                <p className={styles.from}>
+                  hometown<span>兵庫県神戸市</span>
+                </p>
+                <p className={styles.birth}>
+                  date of birth<time dateTime="2005-02-21">2005/02/21</time>
+                </p>
+                <p className={styles.graduation}>Graduation expected in 2026</p>
               </div>
-              <p className={styles.from}>
-                hometown<span>兵庫県神戸市</span>
-              </p>
-              <p className={styles.birth}>
-                date of birth<time dateTime="2005-02-21">2005/02/21</time>
-              </p>
-              <p className={styles.graduation}>Graduation expected in 2026</p>
+            </section>
+            {/* 技術 */}
+            <div className={styles.profileRight}>
+              <section className={styles.skills}>
+                <h2>My skills</h2>
+                <div className={styles.programming}>
+                  <h3>programming language</h3>
+                  <div className={styles.languageWrap}>
+                    <p>html</p>
+                    <p>css</p>
+                    <p>scss</p>
+                    <p>js</p>
+                    <p>react</p>
+                    <p>next.js</p>
+                    <p>pug</p>
+                    <p>PHP</p>
+                    <p>firebase</p>
+                    <p>SQL</p>
+                  </div>
+                </div>
+                <div className={styles.software}>
+                  <h3>software</h3>
+                  <div className={styles.softWrap}>
+                    <div>
+                      <p>Ai</p>
+                    </div>
+                    <div>
+                      <p>Ps</p>
+                    </div>
+                    <div>
+                      <Image src={figma} alt="figmaのアイコン" />
+                    </div>
+                    <div>
+                      <p>Xd</p>
+                    </div>
+                    <div>
+                      <p>Ae</p>
+                    </div>
+                    <div>
+                      <p>Pr</p>
+                    </div>
+                    <div>
+                      <p>Id</p>
+                    </div>
+                  </div>
+                </div>
+              </section>
+              <p className={styles.jobType}>志望職種：デザインエンジニア</p>
             </div>
           </div>
-          {/* <section className={styles.about}>
+          {/* 今までの紹介 */}
+          <section className={styles.about}>
             <h2>about</h2>
+          </section>
+          {/* <div className={styles.}>
             <p>
-              <span>デザインエンジニア志望です</span>
-              <br />
-              UIデザインの知識を持つエンジニアになるために、
+              多くの資格を取得しており、現在はTOEIC600点台を目指して勉強中です
             </p>
-            <div className={styles.detail}>
-              <div className={styles.detailChild}>
-                <h3>coding</h3>
-                <p></p>
-              </div>
-              <div className={styles.detailChild}>
-                <h3>design</h3>
-                <p></p>
-              </div>
-            </div>
-          </section>
-          <section className={styles.skills}>
-            <h2>skills</h2>
-            <div className={styles.skillsWrap}>
-              <div className={styles.engineer}>
-                <h3>programming language</h3>
-                <div className={styles.engWrap}>
-                  <p>html</p>
-                  <p>css</p>
-                  <p>scss</p>
-                  <p>js</p>
-                  <p>react</p>
-                  <p>next.js</p>
-                  <p>firebase</p>
-                </div>
-              </div>
-              <div className={styles.software}>
-                <h3>software</h3>
-                <div className={styles.softWrap}>
-                  <div>
-                    <p>Ai</p>
-                  </div>
-                  <div>
-                    <p>Ps</p>
-                  </div>
-                  <div>
-                    <img src="../../image/figma.png" alt="figmaのアイコン" />
-                  </div>
-                  <div>
-                    <p>Ae</p>
-                  </div>
-                  <div>
-                    <p>Pr</p>
-                  </div>
-                  <div>
-                    <p>Id</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className={styles.certification}>
-              <p>多くの資格を取得しています</p>
-              <p>
-                全商簿記実務検定1級 / 全商英語検定1級 /
-                全商ビジネス計算実務検定試験(電卓)1級 /
-                <br />
-                全商商業経済検定1級 / 全商情報処理検定2級(プログラミング部門) /
-                色彩検定3級
-              </p>
-            </div>
-          </section>
-          <section className={styles.myFav}>
+            <p>
+              全商簿記実務検定1級 / 全商英語検定1級 /
+              全商ビジネス計算実務検定試験(電卓)1級 /<br />
+              全商商業経済検定1級 / 全商情報処理検定2級(プログラミング部門) /
+              色彩検定3級
+            </p>
+          </div> */}
+          {/* 私の好きなもの */}
+          {/* <section className={styles.myFav}>
             <h2>my favorite</h2>
             <div className={styles.childWrap}>
               <div className={styles.favChild}>
-                <div></div>
+                <Image src={ramen} alt="ラーメンのアイコン" />
                 <p>
                   I love ramen!!!
                   <br />
@@ -114,7 +121,7 @@ export default function Profile() {
                 </p>
               </div>
               <div className={styles.favChild}>
-                <div></div>
+                <Image src={music} alt="音楽のアイコン" />
                 <p>
                   listening to pop music
                   <br />
@@ -122,7 +129,7 @@ export default function Profile() {
                 </p>
               </div>
               <div className={styles.favChild}>
-                <div></div>
+                <Image src={paint} alt="パレットのアイコン" />
                 <p>
                   Colored pencil drawing
                   <br />
@@ -130,7 +137,7 @@ export default function Profile() {
                 </p>
               </div>
               <div className={styles.favChild}>
-                <div></div>
+                <Image src={movie} alt="映画のアイコン" />
                 <p>
                   watching movies
                   <br />
--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -1,15 +1,48 @@
+import Image from "next/image";
 import Close from "../components/Close";
 import styles from "./Works.module.scss";
+import logo from "../../../public/img/portfolio_logo.png";
+
+import worksData from "../data/worksData";
 
 export default function Works() {
   return (
     <>
       <div className={styles.windowWrap}>
         <div className={styles.bar}>
-          <h1>Works</h1>
+          <h1>
+            Works (<span>作品紹介</span>)
+          </h1>
           <Close />
         </div>
-        <div className={styles.pageArea}></div>
+        <div className={styles.pageArea}>
+          <header className={styles.header}>
+            {/* 後で変更予定 */}
+            <Image src={logo} alt="ポートフォリオロゴ" />
+            <ul className={styles.nav}>
+              <li>オススメ!</li>
+              <li>受賞歴有</li>
+              <li>すべて</li>
+              <li>個人制作</li>
+              <li>チーム制作</li>
+              <li>グラフィック物</li>
+            </ul>
+          </header>
+          {worksData.map((work) => (
+            <section key={work.id} className={styles.id}>
+              <div className={styles.imgArea}></div>
+              <h2 className={styles.jpn}>
+                {work.title}
+                {work.subtitle && <span>{work.subtitle}</span>}
+              </h2>
+              <p className={styles.explain}>{work.explain}</p>
+              <div className={styles.btmWrap}>
+                <p className={styles.workDate}>{work.workDate}</p>
+                <button>詳細を見る</button>
+              </div>
+            </section>
+          ))}
+        </div>
       </div>
     </>
   );
