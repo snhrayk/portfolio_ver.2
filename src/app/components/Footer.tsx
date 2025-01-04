56180b3b2f4caf89814884e4d08bb42c1e6925b6
snhrayk <ayaka21sunahara@gmail.com>
2025-01-04 22:39:49 +0900

    sns順番入れ替え

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,8 +1,8 @@
 import styles from "./Footer.module.scss";
 import Image from "next/image";
 import insta from "../../../public/img/instagram_footer.svg";
-import github from "../../../public/img/github_footer.svg";
 import x from "../../../public/img/x_footer.svg";
+import github from "../../../public/img/github_footer.svg";
 
 export default function Footer() {
   const link = (event: React.MouseEvent, href: string, windowName: string) => {
@@ -14,14 +14,6 @@ export default function Footer() {
     <div className={styles.borderTop}>
       <div className={styles.footerWrap}>
         <div className={styles.snsWrap}>
-          <a
-            href="https://github.com/snhrayk"
-            onClick={(event) =>
-              link(event, "https://github.com/snhrayk", "私のGitHub")
-            }
-          >
-            <Image src={github} alt="私のGitHub" />
-          </a>
           <a
             href="https://www.instagram.com/a8ksw_/"
             onClick={(event) =>
@@ -41,6 +33,14 @@ export default function Footer() {
             }
           >
             <Image src={x} alt="私のX" />
+          </a>
+          <a
+            href="https://github.com/snhrayk"
+            onClick={(event) =>
+              link(event, "https://github.com/snhrayk", "私のGitHub")
+            }
+          >
+            <Image src={github} alt="私のGitHub" />
           </a>
         </div>
         <p>
