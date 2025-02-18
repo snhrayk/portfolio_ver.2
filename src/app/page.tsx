9c2224904ef4bc793fd7fab13e4a0ef4fe1885c8
snhrayk <ayaka21sunahara@gmail.com>
2025-02-18 16:53:02 +0900

    トップページにメッセージ追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,22 @@
-import styles from "./page.module.css";
+import styles from "./page.module.scss";
 
 export default function Home() {
-  return <div className={styles.page}></div>;
+  return (
+    <div className={styles.page}>
+      <div className={styles.windowWrap}>
+        <div className={styles.bar}>
+          <h1>Welcome to my portfolio &lt;3</h1>
+          {/* <Close /> */}
+        </div>
+        <div className={styles.pageArea}>
+          <p>
+            画面右にあるアイコンをクリックすると
+            <br />
+            各ページに遷移できます
+          </p>
+          <span>※Contactをクリックするとメールが起動します</span>
+        </div>
+      </div>
+    </div>
+  );
 }
