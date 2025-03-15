a906eba33d7e335fe10022a2ddc025d7562e5aa4
snhrayk <ayaka21sunahara@gmail.com>
2025-03-15 23:23:54 +0900

    モーダルウィンドウ一旦後回し

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,62 +1,12 @@
-// "use client";
-
-// import { useEffect, useState } from "react";
-// import { useRouter } from "next/navigation";
-// import Msg from "./components/Msg";
-// import styles from "./page.module.scss";
-
-// export default function Home() {
-//   const [isFirstVisit, setIsFirstVisit] = useState(true);
-//   const router = useRouter();
-
-//   useEffect(() => {
-//     const hasVisited = localStorage.getItem("hasVisited");
-
-//     if (!hasVisited) {
-//       // 初回訪問なら、localStorageに保存 & /profile にリダイレクト
-//       localStorage.setItem("hasVisited", "true");
-//       setIsFirstVisit(true);
-//       router.replace("/profile");
-//     } else {
-//       setIsFirstVisit(false);
-//     }
-//   }, [router]);
-
-//   return (
-//     <div className={styles.page}>
-//       {isFirstVisit ? <span>{"</Msg>"}</span> : <Msg />}
-//     </div>
-//   );
-// }
-
 "use client";
 
-import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
 import Msg from "./components/Msg";
 import styles from "./page.module.scss";
 
 export default function Home() {
-  const [isFirstVisit, setIsFirstVisit] = useState(true);
-  const router = useRouter();
-
-  useEffect(() => {
-    const hasVisited = localStorage.getItem("hasVisited");
-
-    if (!hasVisited) {
-      // 初回訪問なら、localStorageに保存して、/profileページへリダイレクト
-      localStorage.setItem("hasVisited", "true");
-      setIsFirstVisit(true);
-      router.replace("/profile");
-    } else {
-      setIsFirstVisit(false);
-    }
-  }, [router]);
-
   return (
     <div className={styles.page}>
-      {isFirstVisit && <span className={styles.msgOverlay}>{"</Msg>"}</span>}
-      {!isFirstVisit && <Msg />}
+      <Msg />
     </div>
   );
 }
